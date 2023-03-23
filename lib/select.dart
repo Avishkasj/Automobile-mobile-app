973745b35d3253523e0bd9f06d21ad2ac7f57346
Firstname Lastname <asupunjayasooriya@gmail.com>
2023-03-23 22:16:43 +0530

    Update select.dart

--- lib/select.dart
+++ lib/select.dart
@@ -46,30 +46,74 @@ class _selectState extends State<select> {
         ),
       ),
       backgroundColor: Colors.black,
-      body: Container(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 10),
-              child: Opacity(
-                opacity: 1,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(10),
-                  child: Image.asset(
-                    'assets/img3.png',
-                    width: 600,
-                    height: 200,
-                    fit: BoxFit.cover,
+      body: SingleChildScrollView(
+        child: Container(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child: Opacity(
+                  opacity: 1,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Image.asset(
+                      'assets/img3.png',
+                      width: 600,
+                      height: 200,
+                      fit: BoxFit.cover,
+                    ),
                   ),
                 ),
               ),
-            ),
 
-            //dropdown vehicale type
+              //dropdown vehicale type
 
-            Padding(
-              padding: const EdgeInsets.only(top: 10),
-              child: Theme(
+              Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child: Theme(
+                  data: ThemeData(
+                    colorScheme: ColorScheme.light(
+                      primary: Colors.white, // set the primary color to white
+                      onSurface: Colors.white, // set the onSurface color to white
+                    ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: DropdownButtonFormField(
+                      value: dropdownValue,
+                      icon: const Icon(Icons.arrow_downward),
+                      elevation: 16,
+                      style: const TextStyle(color: Colors.blueAccent),
+                      onChanged: (String? value) {
+                        setState(() {
+                          dropdownValue = value!;
+                        });
+                      },
+                      items: list.map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                      decoration: InputDecoration(
+                        labelText: "Damage Type",
+                        prefixIcon: Icon(
+                          Icons.car_crash,
+                          color: Colors.white,
+                        ),
+                        border: OutlineInputBorder(
+                          borderSide: const BorderSide(width: 1.0),
+                        ),
+                        labelStyle: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+
+              //dropdown damage
+
+              Theme(
                 data: ThemeData(
                   colorScheme: ColorScheme.light(
                     primary: Colors.white, // set the primary color to white
@@ -79,16 +123,16 @@ class _selectState extends State<select> {
                 child: Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: DropdownButtonFormField(
-                    value: dropdownValue,
+                    value: dropdownValue2,
                     icon: const Icon(Icons.arrow_downward),
                     elevation: 16,
                     style: const TextStyle(color: Colors.blueAccent),
-                    onChanged: (String? value) {
+                    onChanged: (String? value2) {
                       setState(() {
-                        dropdownValue = value!;
+                        dropdownValue2 = value2!;
                       });
                     },
-                    items: list.map<DropdownMenuItem<String>>((String value) {
+                    items: list2.map<DropdownMenuItem<String>>((String value) {
                       return DropdownMenuItem<String>(
                         value: value,
                         child: Text(value),
@@ -97,7 +141,7 @@ class _selectState extends State<select> {
                     decoration: InputDecoration(
                       labelText: "Damage Type",
                       prefixIcon: Icon(
-                        Icons.car_crash,
+                        Icons.auto_fix_high_outlined,
                         color: Colors.white,
                       ),
                       border: OutlineInputBorder(
@@ -108,179 +152,163 @@ class _selectState extends State<select> {
                   ),
                 ),
               ),
-            ),
 
-            //dropdown damage
-
-            Theme(
-              data: ThemeData(
-                colorScheme: ColorScheme.light(
-                  primary: Colors.white, // set the primary color to white
-                  onSurface: Colors.white, // set the onSurface color to white
-                ),
-              ),
-              child: Padding(
+              Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: DropdownButtonFormField(
-                  value: dropdownValue2,
-                  icon: const Icon(Icons.arrow_downward),
-                  elevation: 16,
-                  style: const TextStyle(color: Colors.blueAccent),
-                  onChanged: (String? value2) {
-                    setState(() {
-                      dropdownValue2 = value2!;
-                    });
-                  },
-                  items: list2.map<DropdownMenuItem<String>>((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  }).toList(),
-                  decoration: InputDecoration(
-                    labelText: "Damage Type",
-                    prefixIcon: Icon(
-                      Icons.auto_fix_high_outlined,
-                      color: Colors.white,
-                    ),
-                    border: OutlineInputBorder(
-                      borderSide: const BorderSide(width: 1.0),
-                    ),
-                    labelStyle: TextStyle(color: Colors.white),
+                child: Container(
+                  color: Colors.white30,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Container(
+                        child: Row(
+                          children: [
+                            Radio(
+                              value: 1,
+                              groupValue: _selectedValue,
+                              onChanged: (value) {
+                                setState(() {
+                                  _selectedValue = value as int;
+                                });
+                              },
+                              activeColor: Colors.white,
+                            ),
+                            Text(
+                              "Petrol",
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        child: Row(
+                          children: [
+                            Radio(
+                              value: 2,
+                              groupValue: _selectedValue,
+                              onChanged: (value) {
+                                setState(() {
+                                  _selectedValue = value as int;
+                                });
+                              },
+                              activeColor: Colors.white,
+                            ),
+                            Text(
+                              "Diesel",
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        child: Row(
+                          children: [
+                            Radio(
+                              value: 3,
+                              groupValue: _selectedValue,
+                              onChanged: (value) {
+                                setState(() {
+                                  _selectedValue = value as int;
+                                });
+                              },
+                              activeColor: Colors.white,
+                            ),
+                            Text(
+                              "Electrical",
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ),
-            ),
 
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                color: Colors.white30,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Container(
-                      child: Row(
-                        children: [
-                          Radio(
-                            value: 1,
-                            groupValue: _selectedValue,
-                            onChanged: (value) {
-                              setState(() {
-                                _selectedValue = value as int;
-                              });
-                            },
-                            activeColor: Colors.white,
-                          ),
-                          Text(
-                            "Petrol",
-                            style: TextStyle(
-                              color: Colors.white,
-                            ),
-                          ),
-                        ],
+              // Row(
+              //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              //   children: [
+              //     Padding(
+              //       padding: const EdgeInsets.all(8.0),
+              //       child: Container(
+              //         color: Colors.white,
+              //         height: 185,
+              //         width: 185,
+              //         child: Text('test'),
+              //       ),
+              //     ),
+              //
+              //     Padding(
+              //       padding: const EdgeInsets.all(8.0),
+              //       child: Container(
+              //         color: Colors.white30,
+              //         height: 185,
+              //         width: 185,
+              //         child: Text('test'),
+              //       ),
+              //     )
+              //   ],
+              // ),
+
+              SizedBox(height: 240,),
+              //next button
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: OutlinedButton(
+                      child: Text(
+                        "Reset",
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
                       ),
-                    ),
-                    Container(
-                      child: Row(
-                        children: [
-                          Radio(
-                            value: 2,
-                            groupValue: _selectedValue,
-                            onChanged: (value) {
-                              setState(() {
-                                _selectedValue = value as int;
-                              });
-                            },
-                            activeColor: Colors.white,
-                          ),
-                          Text(
-                            "Diesel",
-                            style: TextStyle(
-                              color: Colors.white,
-                            ),
-                          ),
-                        ],
+                      style: OutlinedButton.styleFrom(
+                        side: BorderSide(
+                          width: 2.0, // set the border weight to 2.0
+                          color: Colors.white,
+                        ),
+                        fixedSize: Size(185, 50),
                       ),
+                      onPressed: () {
+                        // Navigator.push(context,
+                        //     MaterialPageRoute(builder: (context) => select()));
+                      },
                     ),
-                    Container(
-                      child: Row(
-                        children: [
-                          Radio(
-                            value: 3,
-                            groupValue: _selectedValue,
-                            onChanged: (value) {
-                              setState(() {
-                                _selectedValue = value as int;
-                              });
-                            },
-                            activeColor: Colors.white,
-                          ),
-                          Text(
-                            "Electrical",
-                            style: TextStyle(
-                              color: Colors.white,
-                            ),
-                          ),
-                        ],
+                  ),
+
+                  Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: OutlinedButton(
+                      child: Text(
+                        "Next",
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
                       ),
+                      style: OutlinedButton.styleFrom(
+                        side: BorderSide(
+                          width: 2.0, // set the border weight to 2.0
+                          color: Colors.white,
+                        ),
+                        fixedSize: Size(185, 50),
+                      ),
+                      onPressed: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) => select()));
+                      },
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-            ),
-
-            //next button
-            Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(15.0),
-                  child: OutlinedButton(
-                    child: Text(
-                      "Reset",
-                      style: TextStyle(
-                        color: Colors.white,
-                      ),
-                    ),
-                    style: OutlinedButton.styleFrom(
-                      side: BorderSide(
-                        width: 2.0, // set the border weight to 2.0
-                        color: Colors.white,
-                      ),
-                      fixedSize: Size(185, 50),
-                    ),
-                    onPressed: () {
-                      // Navigator.push(context,
-                      //     MaterialPageRoute(builder: (context) => select()));
-                    },
-                  ),
-                ),
-
-                Padding(
-                  padding: const EdgeInsets.all(15.0),
-                  child: OutlinedButton(
-                    child: Text(
-                      "Next",
-                      style: TextStyle(
-                        color: Colors.white,
-                      ),
-                    ),
-                    style: OutlinedButton.styleFrom(
-                      side: BorderSide(
-                        width: 2.0, // set the border weight to 2.0
-                        color: Colors.white,
-                      ),
-                      fixedSize: Size(185, 50),
-                    ),
-                    onPressed: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => select()));
-                    },
-                  ),
-                ),
-              ],
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
