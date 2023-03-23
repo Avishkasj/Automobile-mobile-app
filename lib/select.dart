8dac2c3f4cb18c5b202901107af31ed1d8fd17d9
Firstname Lastname <asupunjayasooriya@gmail.com>
2023-03-23 20:04:56 +0530

    select

--- lib/select.dart
+++ lib/select.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+const List<String> list = <String>[' Cars/Vans/ Cabs and Jeeps ', 'Three wheelers', 'Motor Bikes', 'Busses/ Lorries'];
+
 class select extends StatefulWidget {
   const select({Key? key}) : super(key: key);
 
@@ -8,26 +10,121 @@ class select extends StatefulWidget {
 }
 
 class _selectState extends State<select> {
+  String dropdownValue = list.first;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
         iconTheme: IconThemeData(color: Colors.black),
-        title: Text("Select",
-        style:TextStyle(
-          color: Colors.black,
-        ),),
+        title: Text(
+          "Select",
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
       ),
       backgroundColor: Colors.black,
       body: Container(
         child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 10),
+              child: Opacity(
+                opacity: 1,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image.asset(
+                    'assets/img3.png',
+                    width: 600,
+                    height: 200,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
 
-          
+            //dropdown vehicale type
+
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: DropdownButtonFormField(
+                value: dropdownValue,
+                icon: const Icon(Icons.arrow_downward),
+                elevation: 16,
+                style: const TextStyle(color: Colors.blueAccent),
+                onChanged: (String? value) {
+                  // This is called when the user selects an item.
+                  setState(() {
+                    dropdownValue = value!;
+                  });
+                },
+                items: list.map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+                decoration: InputDecoration(
+                  labelText: "Vehicle Type",
+                  prefixIcon: Icon(
+                    Icons.car_crash,
+                    color: Colors.white,
+                  ),
+                  border: OutlineInputBorder(
+                    borderSide: const BorderSide(color: Colors.grey, width: 0.0),
+                  ),
+                  labelStyle: new TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
 
 
+            //dropdown damage
+
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: DropdownButtonFormField(
+                value: dropdownValue,
+                icon: const Icon(Icons.arrow_downward),
+                elevation: 16,
+                style: const TextStyle(color: Colors.blueAccent),
+                onChanged: (String? value) {
+                  // This is called when the user selects an item.
+                  setState(() {
+                    dropdownValue = value!;
+                  });
+                },
+                items: list.map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+                decoration: InputDecoration(
+                  labelText: "Damage Type",
+                  prefixIcon: Icon(
+                    Icons.car_crash,
+                    color: Colors.white,
+                  ),
+                  border: OutlineInputBorder(
+                    borderSide: const BorderSide(color: Colors.grey, width: 0.0),
+                  ),
+                  labelStyle: new TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+
+            Text(
+              'Hello, world!',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24,
+              ),
+            ),
+          ],
         ),
-      ) ,
+      ),
     );
   }
 }
